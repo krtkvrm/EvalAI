bf78d6edbabe596f8e46d4b53ac419651d506d53
Kartik Verma <vkartik97@gmail.com>
2019-02-19 02:49:34 +0530

    Test4

--- tests/unit/jobs/test_views.py
+++ tests/unit/jobs/test_views.py
@@ -904,6 +904,10 @@ class GetRemainingSubmissionTest(BaseAPITestClass):
         setattr(self.challenge_phase, 'max_submissions_per_day', 15)
         setattr(self.challenge_phase, 'max_submissions_per_month', 13)
         self.challenge_phase.save()
+        self.submission3.status = 'cancelled'
+        self.submission2.status = 'failed'
+        self.submission3.save()
+        self.submission2.save()
         expected = {
             'participant_team': self.participant_team.team_name,
             'participant_team_id': self.participant_team.id,
@@ -913,9 +917,9 @@ class GetRemainingSubmissionTest(BaseAPITestClass):
                 'start_date': self.challenge_phase.start_date,
                 'end_date': self.challenge_phase.end_date,
                 'message': {
-                    'remaining_submissions_today_count': 11,
-                    'remaining_submissions_this_month_count': 11,
-                    'remaining_submissions': 98
+                    'remaining_submissions_today_count': 12,
+                    'remaining_submissions_this_month_count': 12,
+                    'remaining_submissions': 99
                 }
             }]
         }
