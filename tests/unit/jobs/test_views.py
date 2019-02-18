8925f8d9a7f1c1f2cfd0e4a0a9932a846dfee036
Kartik Verma <vkartik97@gmail.com>
2019-02-19 02:40:22 +0530

    Test2

--- tests/unit/jobs/test_views.py
+++ tests/unit/jobs/test_views.py
@@ -366,7 +366,6 @@ class BaseAPITestClass(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.challenge_phase.max_submissions = actual_maxinmum_submissions
         self.challenge_phase.save()
-    self.self.maxDiff = None
     def test_challenge_submission_for_docker_based_challenges(self):
         self.url = reverse_lazy('jobs:challenge_submission',
                                 kwargs={'challenge_id': self.challenge.pk,
@@ -375,7 +374,7 @@ class BaseAPITestClass(APITestCase):
         self.challenge.participant_teams.add(self.participant_team)
         self.challenge.is_docker_based = True
         self.challenge.save()
-
+        self.self.maxDiff = None
         response = self.client.post(self.url, {
                                         'status': 'submitting', 'input_file': self.input_file}, format="multipart")
         expected = {
